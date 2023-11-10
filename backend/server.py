857f46172c0dd851834466833515ec21429d4fde
snrathodk <shan_rathod@ymail.com>
2023-11-10 12:57:37 +0530

    Carousel  operations have done on POC bases

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, jsonify, request
 # Import flask and datetime module for showing date and time
 import datetime
 
@@ -20,7 +20,12 @@ def get_time():
 		"programming":"python"
 		}
 
-	
+@app.route("/api_endpoint", methods=["POST"])
+def function_for_api():
+    img = request.files['file']
+    print(img.filename)
+    return Response(status=200)
+
 # Running app
 if __name__ == '__main__':
 	app.run(debug=True, port=5001)
